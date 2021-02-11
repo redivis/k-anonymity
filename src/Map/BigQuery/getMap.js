5781f5b6c55666ddf24f9797d327494e0c62e46f
imathews <imathews@users.noreply.github.com>
2021-02-11 03:26:34 +0000

    Deploying to gh-pages from @ redivis/geo-coverage@70af89058041e1b70ff623a004e7ba0fbf6fbed9 🚀

---
+++ src/Map/BigQuery/getMap.js
@@ -0,0 +1,103 @@
+import * as topojson from 'topojson';
+import * as topojsonClient from 'topojson-client';
+import retryableFetch from '../../helpers/retryableFetch';
+
+import buildTopology from './buildTopology';
+import getRoads from './getRoads';
+import executeQuery from './executeQuery';
+import getBoundaryStringArray from './getBoundaryStringArray';
+import mapRasterLoader from './mapRasterLoader';
+import getSublevels from './getSublevels';
+import * as cache from '../../helpers/cache';
+
+/**
+ * @param queryInfo
+ *    region: STR,
+ *    subregion: STR, // optional
+ *    roadsFromOtherRegions: ARR(STR region) -> to include nearby roads
+ *    [roads] : ARRAY(STR IN [motorway, trunk, primary, secondary, tertiary, unclassified, residential, surface, ...])
+ * @returns {Promise.<*>}
+ */
+export default async function (queryInfo) {
+	if (!queryInfo || !queryInfo.region) {
+		throw new Error(`Invalid query info:\n${JSON.stringify(queryInfo)}`);
+	} else {
+		const adminLevel = queryInfo.subregion ? 4 : 2;
+		const subLevels = queryInfo.subregion ? [6] : [4];
+		const topologyMap = {};
+		let boundaryQuery = '[out:json];';
+
+		if (queryInfo.subregion) {
+			boundaryQuery += `area[name="${queryInfo.region}"];(`;
+		}
+		boundaryQuery += `relation ["boundary"] ["admin_level"="${adminLevel}"][~"^name"~"^${
+			queryInfo.subregion || queryInfo.region
+		}$"]`;
+
+		if (queryInfo.subregion) {
+			boundaryQuery += `(area);(._;>;););out;`;
+		} else {
+			boundaryQuery += '; (._;>;); out body;';
+		}
+
+		const res = await executeQuery(boundaryQuery);
+
+		if (res.features.length === 0) {
+			await cache.unset(boundaryQuery);
+			throw new Error('OSM returned an empty response');
+		}
+
+		let boundaryStringArray;
+
+		topologyMap.boundary = res;
+
+		boundaryStringArray = getBoundaryStringArray(topologyMap.boundary);
+
+		await getSublevels(subLevels, boundaryStringArray, topologyMap);
+		await getRoads(queryInfo.roads, boundaryStringArray, topologyMap);
+
+		let topology;
+		if (queryInfo.roadsFromOtherRegions) {
+			topology = buildTopology(JSON.parse(JSON.stringify(topologyMap)));
+		} else {
+			topology = buildTopology(topologyMap);
+		}
+		const rasters = await mapRasterLoader(topology.bbox, ['population_density']);
+
+		if (queryInfo.roadsFromOtherRegions) {
+			console.info(`other regions: ${queryInfo.roadsFromOtherRegions}`);
+
+			for (const region of queryInfo.roadsFromOtherRegions) {
+				console.info(`getting roads for ${region}`);
+				let regionBoundaryQuery = '[out:json];';
+
+				if (queryInfo.subregion) {
+					regionBoundaryQuery += `area[name="${queryInfo.region}"];(`;
+				}
+				regionBoundaryQuery += `relation ["boundary"] ["admin_level"="${adminLevel}"][~"^name"~"^${region}$"]`;
+
+				if (queryInfo.subregion) {
+					regionBoundaryQuery += `(area);(._;>;););out;`;
+				} else {
+					regionBoundaryQuery += '; (._;>;); out body;';
+				}
+				const boundaryQueryResponse = await executeQuery(regionBoundaryQuery);
+
+				if (res.features.length === 0) {
+					await cache.unset(regionBoundaryQuery);
+					throw new Error('OSM returned an empty response');
+				}
+				console.info(`got boundary for ${region}`);
+
+				const boundaryStringArray = getBoundaryStringArray(boundaryQueryResponse);
+				await getRoads(queryInfo.roads, boundaryStringArray, topologyMap, region);
+				console.info(`got roads for ${region}`);
+			}
+			topology = buildTopology(topologyMap);
+		}
+
+		topology.rasters = rasters;
+
+		return topology;
+	}
+}
